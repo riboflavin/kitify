f352dd63587cd49b1f1abfa632a6fe62788879f7
riboflavin <justin@10gen>
2015-02-13 11:07:24 -0500

    change serve static assets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Kitify::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -47,6 +47,4 @@ Kitify::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # serve static files
-  config.serve_static_files = true
 end
